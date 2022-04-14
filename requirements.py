91ba0b3fb0eb00dadef832042b8421ef245b4056
Tahg <103221002+T4hg@users.noreply.github.com>
2022-04-14 12:16:23 +0200

    Requirements

---
+++ requirements.py
@@ -0,0 +1,29 @@
+import os
+try:
+    os.system("pip install colorama")
+except:
+    pass
+try:
+    os.system("pip install json")
+except:
+    pass
+try:
+    os.system("pip install requests")
+except:
+    pass
+try:
+    os.system("pip install datetime")
+except:
+    pass
+try:
+    os.system("pip install getpass4")
+except:
+    pass
+try:
+    os.system("pip install ctypes")
+except:
+    pass
+try:
+    os.system("pip install getpass")
+except:
+    pass
